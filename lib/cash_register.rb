d7d4ae0067b25d5ecf74b90b60f4a7cd7f676da1
Chris Baum <christopher.baum@valpo.edu>
2021-01-29 02:11:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     new_item = (price*quantity) #need to save new_item seperately from @total so it isn't associate with the instance variable
     @last_transaction = new_item
-    @total = @total + new_item
+    self.total = self.total + new_item
     quantity.times {@item_array << title}
   end
 
