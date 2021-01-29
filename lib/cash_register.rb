09c2cbc64ce364926c600293d3d264cbaf29219a
Chris Baum <christopher.baum@valpo.edu>
2021-01-29 02:12:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,17 +11,17 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     new_item = (price*quantity) #need to save new_item seperately from @total so it isn't associate with the instance variable
-    @last_transaction = new_item
+    self.last_transaction = new_item
     self.total = self.total + new_item
     quantity.times {@item_array << title}
   end
 
   def apply_discount
-    if @discount == 0
+    if self.discount == 0
       "There is no discount to apply."
     else
-      @total = @total.to_f*((100.0 - @discount.to_f)/100.0)
-      "After the discount, the total comes to $#{@total.to_i}."
+      self.total = self.total.to_f*((100.0 - self.discount.to_f)/100.0)
+      "After the discount, the total comes to $#{self.total.to_i}."
     end
   end
 
